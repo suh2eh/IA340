0b93e427c34e5917b391ef99c4fdf8d5beb53bc1
suh2eh <143033804+suh2eh@users.noreply.github.com>
2023-09-20 15:45:34 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -55,7 +55,13 @@ where p_email ='p1@jmu.edu'
 --Q2.7
 select * from enroll
 --Q2.8
-
+	
+select c_number ,count(*) as num_student
+from enroll
+group by c_number
+order by num_student desc
+limit 1
+	
 --Q2.9
 select professor.p_name,
 		course.c_name
