cf14cde22334164519f327aa38efd04fc22d95e3
suh2eh <143033804+suh2eh@users.noreply.github.com>
2023-10-02 15:56:07 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -16,11 +16,26 @@ conn = psycopg2.connect(host = host,
                        dbname = db)
 
 
-#Q1
+--Q1
 sql = """ select * from student"""
 
 df = pandas.read_sql_query(sql, conn)
 df[:]
 
-#Q2
+--Q2
+
+  sql = """select professor.p_name, course.c_name
+        from professor
+        inner join course
+        on professor.p_email = course.p_email"""
+  
+  df = pandas.read_sql_query(sql, conn)
+  df[:10]
+  
+--Would the following code work as well?
+sql = """select p_name, c_name
+        from professor, course"""
+  
+df = pandas.read_sql_query(sql, conn)
+df[:10]
 
