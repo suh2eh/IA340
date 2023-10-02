3f33dee409b86e36709b6d93f29d30dddb9f0ce4
suh2eh <143033804+suh2eh@users.noreply.github.com>
2023-10-02 16:27:50 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -39,3 +39,45 @@ sql = """select p_name, c_name
 df = pandas.read_sql_query(sql, conn)
 df[:10]
 
+--Q3
+
+sql = """select c_number, count(*) as num_student
+        from enroll
+        group by c_number
+        order by num_student desc"""
+  
+df = pandas.read_sql_query(sql, conn)
+df.plot.bar(x='c_number',y='num_student')
+
+--Q4
+
+sql = """select professor.p_name, count(course.c_number) as num_courses
+        from professor
+        inner join course on professor.p_email = course.p_email
+        group by professor.p_name
+        order by num_courses desc"""
+
+df = pandas.read_sql_query(sql, conn)
+df.plot.bar(x='p_name',y='num_courses')
+
+--Q5
+cur = conn.cursor()
+sql = """insert into professor(p_email,p_name,office)
+        values('p4@jmu.edu','p4','o4')"""
+cur.execute(sql)
+cur.commit()
+df = pandas.read_sql_query('select * from professor', conn)
+df[:]
+
+sql = """insert into course(c_number,c_name,room,p_email)
+        values('c5','linkedin','r2','p4@jmu.edu')"""
+cur.execute(sql)
+cur.commit()
+df = pandas.read_sql_query('select * from course', conn)
+df[:]
+
+
+
+
+
+
